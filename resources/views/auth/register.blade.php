8cfb51d7ae869f7fa17de1cc0cd7dfaab7eba6c9
femartinez1 <femartinez@ikeasistencia.com>
2025-01-30 17:14:18 -0600

    add funcionalidad de registrar usuario

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@ Registrate en DevStagram
             <img src="{{ asset('img/registrar.jpg')}}" alt="Imagen Registro de Usuarios ">
         </div>
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
-            <form action="{{ route('register') }}" method="POST">
+            <form action="{{ route('register') }}" method="POST" novalidate>
                 @csrf
                 <div class="mb-5">
                     <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
@@ -21,7 +21,9 @@ Registrate en DevStagram
                         name = "name"
                         type = "text"
                         placeholder = "Tu nombre"
-                        class = "border p-3 w-full rounded-lg"
+                        class = "border p-3 w-full rounded-lg @error('name') border-red-500
+                        @enderror"
+                        value="{{ old('name') }}"
                     />
                     @error('name')
                         <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{ $message }}</p>
@@ -36,8 +38,13 @@ Registrate en DevStagram
                         name = "username"
                         type = "text"
                         placeholder = "Tu nombre de usuario"
-                        class = "border p-3 w-full rounded-lg"
+                        class = "border p-3 w-full rounded-lg @error('username') border-red-500
+                        @enderror"
+                        value="{{ old('username') }}"
                     />
+                    @error('username')
+                        <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{ $message }}</p>
+                    @enderror  
                 </div>
                 <div class="mb-5">
                     <label for="email"  class="mb-2 block uppercase text-gray-500 font-bold">
@@ -48,8 +55,13 @@ Registrate en DevStagram
                         name = "email"
                         type = "email"
                         placeholder = "Tu Email de registro"
-                        class = "border p-3 w-full rounded-lg"
+                        class = "border p-3 w-full rounded-lg @error('email') border-red-500
+                        @enderror"
+                        value="{{ old('email') }}"
                     />
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="password"  class="mb-2 block uppercase text-gray-500 font-bold">
@@ -60,16 +72,20 @@ Registrate en DevStagram
                         name = "password"
                         type = "password"
                         placeholder = "Password de registro"
-                        class = "border p-3 w-full rounded-lg"
+                        class = "border p-3 w-full rounded-lg @error('password') border-red-500
+                        @enderror"
                     />
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-5">
-                    <label for="password_confimation"  class="mb-2 block uppercase text-gray-500 font-bold">
-                        Password
+                    <label for="password_confirmation"  class="mb-2 block uppercase text-gray-500 font-bold">
+                        Repetir Password
                     </label>
                     <input 
-                        id = "password_confimation"
-                        name = "password_confimation"
+                        id = "password_confirmation"
+                        name = "password_confirmation"
                         type = "password"
                         placeholder = "Repite tu Password"
                         class = "border p-3 w-full rounded-lg"
