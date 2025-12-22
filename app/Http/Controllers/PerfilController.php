565ab893aa7ee7571b59c60a95baaedc4e78edad
femartinez1 <femartinez@ikeasistencia.com>
2025-12-22 13:49:53 -0600

    add funcionalidad para editar perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -13,5 +17,51 @@ class PerfilController extends Controller
     public function index()
     {
         return view('perfil.index');
+    }
+    public function store(Request $request)
+    {
+        // Modificar el request
+        $request->request->add(['username' => Str::slug($request->username)]);
+        $this->validate($request, [
+            'username' => ['required', 'min:3', 'max:20', 'unique:users,username,'.auth()->user()->id, 'not_in:twitter,editar-perfil'],
+            'email' => ['required', 'email', 'max:60', 'unique:users,email,'.auth()->user()->id],
+            'password' => ['nullable', 'required_with:nuevo_password', 'current_password'],
+            'nuevo_password' => ['nullable', 'min:6', 'confirmed']
+        ],
+        [
+            'username.required' => 'El nombre de usuario es obligatorio',
+            'username.unique' => 'El nombre de usuario ya está en uso',
+            'username.min' => 'El nombre de usuario debe tener al menos 3 caracteres',
+            'username.max' => 'El nombre de usuario no debe exceder los 20 caracteres',
+            'username.not_in' => 'El nombre de usuario no está permitido',
+            'email.required' => 'El correo electrónico es obligatorio',
+            'email.email' => 'Debe ingresar un correo electrónico válido',
+            'email.unique' => 'El correo electrónico ya está en uso',
+            'email.max' => 'El correo electrónico no debe exceder los 60 caracteres',
+            'password.current_password' => 'La contraseña actual es incorrecta',
+            'nuevo_password.min' => 'La nueva contraseña debe tener al menos 6 caracteres',
+            'nuevo_password.confirmed' => 'La confirmación de la nueva contraseña no coincide'
+        ]);
+        if($request->imagen) {
+            $imagen = $request->file('imagen');
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+            $imagenServidor = Image::make($imagen);
+            $imagenServidor->fit(1000, 1000);
+
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+
+        }
+        $nuevo_password = Hash::make($request->nuevo_password);
+        // Guardar los cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->email = $request->email;
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? '';
+        $usuario->password = $nuevo_password ?? auth()->user()->password;
+        $usuario->save();
+        //redireccionar
+        return redirect()->route('posts.index', $usuario->username);
     }
 }
--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -7,7 +7,8 @@
 @section('contenido')
     <div class="md:flex md:justify-center">
         <div class="md:w-1/2 bg-white shadow p-6">
-            <form action="" class="mt-10 md:mt-0">
+            <form method="POST" action="{{ route('perfil.store') }}" enctype="multipart/form-data" class="mt-10 md:mt-0">
+                @csrf
                 <div class="mb-5">
                     <label for="username"  class="mb-2 block uppercase text-gray-500 font-bold">
                         Username
@@ -26,6 +27,23 @@
                     @enderror  
                 </div>
                 <div class="mb-5">
+                    <label for="email"  class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input 
+                        id = "email"
+                        name = "email"
+                        type = "email"
+                        placeholder = "Tu Email de registro"
+                        class = "border p-3 w-full rounded-lg @error('email') border-red-500
+                        @enderror"
+                        value="{{ auth()->user()->email }}"
+                    />
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
                     <label for="imagen"  class="mb-2 block uppercase text-gray-500 font-bold">
                         Imagen de Perfil
                     </label>
@@ -35,6 +53,50 @@
                         type = "file"
                         class = "border p-3 w-full rounded-lg"
                         accept=".jpg, .jpeg, png"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password"  class="mb-2 block uppercase text-gray-500 font-bold">
+                        Password
+                    </label>
+                    <input 
+                        id = "password"
+                        name = "password"
+                        type = "password"
+                        placeholder = "Password de registro"
+                        class = "border p-3 w-full rounded-lg @error('password') border-red-500
+                        @enderror"
+                    />
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="nuevo_password"  class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nuevo Password
+                    </label>
+                    <input 
+                        id = "nuevo_password"
+                        name = "nuevo_password"
+                        type = "password"
+                        placeholder = "Ingresa tu nuevo password"
+                        class = "border p-3 w-full rounded-lg @error('nuevo_password') border-red-500
+                        @enderror"
+                    />
+                    @error('nuevo_password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="nuevo_password_confirmation"  class="mb-2 block uppercase text-gray-500 font-bold">
+                        Confirma tu nuevo password
+                    </label>
+                    <input 
+                        id = "nuevo_password_confirmation"
+                        name = "nuevo_password_confirmation"
+                        type = "password"
+                        placeholder = "Confirma tu nuevo password"
+                        class = "border p-3 w-full rounded-lg"
                     />
                 </div>
                 <input 
